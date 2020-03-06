4d99e47dfe88613dff1b334ace965e9e32276953
Milos Miric <milosrt6917@gs.viser.edu.rs>
2020-03-07 00:19:28 +0100

    Refaktor EmailService klase

--- IdentityDemo/Users/Infrastructure/EmailService.cs
+++ IdentityDemo/Users/Infrastructure/EmailService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -13,12 +14,12 @@ namespace Users.Infrastructure
     {
         public Task SendAsync(IdentityMessage message)
         {
-            var sendGridUserName = "apikey";
-            var sentFrom = "miricja@gmail.com";
-            var sendGridPassword = "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";
+            string sendGridUserName  = "apikey";
+            string sentFrom          = "miricja@gmail.com";
+            string sendGridPassword  = ConfigurationManager.AppSettings["SendGridApiKey"];
 
             // Konfiguracija klijenta
-            var client = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+            var client = new SmtpClient(host: "smtp.sendgrid.net", port: 587);
 
             client.Port = 587;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
@@ -29,9 +30,9 @@ namespace Users.Infrastructure
             client.EnableSsl = true; client.Credentials = credentials;
 
             // Kreiranje poruke
-            var mail = new MailMessage(sentFrom, message.Destination); 
-            mail.Subject = message.Subject;
-            mail.Body = message.Body;
+            var mail      = new MailMessage(sentFrom, message.Destination);
+            mail.Subject  = message.Subject;
+            mail.Body     = message.Body;
 
             // Slanje
             return client.SendMailAsync(mail);
